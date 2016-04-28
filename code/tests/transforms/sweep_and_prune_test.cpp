879d1e03c041f8ab5e8ea2685b66ad192e170332
Phil Cooper-King <phil@cooperking.net>
2016-04-28 21:33:43 +0100

    moved some stuff around.

---
+++ code/tests/transforms/sweep_and_prune_test.cpp
@@ -0,0 +1,29 @@
+// Sweep and Prune test
+#include <catch/catch.hpp>
+
+
+
+TEST_CASE("Sweep and Prune removes overlapping aabbs")
+{
+
+  SECTION("Sweep should pickup all entites")
+  {
+  }
+
+
+  SECTION("Prune should prune no entities")
+  {
+  }
+
+
+  SECTION("Prune should pull some entities")
+  {
+  }
+
+
+  SECTION("Prune should remove all entities")
+  {
+  }
+
+
+}
